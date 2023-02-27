9e179cbfa1fee898de6e2acad5b960743c9110ac
meysam <rahmanian.meysam655@gmail.com>
2023-02-26 17:52:16 -0700

    Add project files.

---
+++ PipePressureDrop/ConversionFactor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipePressureDrop
+{
+    public static class ConversionFactor
+    {
+        public const double BBL_TO_FT3 = 5.6145835124493;           // unit conversion from barrel to cubic feet
+        public const double BBL_TO_M3 = 0.1589873;                  // unit conversion from barrel to cubic meter
+        public const double FT3_TO_M3 = 0.0283168;                  // unit conversion from cubic feet to cubic meter
+        public const double CP_TO_LB_FTSec = 0.000671968994813;     // unit conversion from cp to lb/(ft.sec)
+        public const double CP_TO_KG_MSec = 0.001;                  // unit conversion from cp to kg/(m.sec)
+        public const double IN_TO_MM = 25.4;                        // unit conversion from inch to mm
+        public const double FT_TO_M = 0.3048;                       // unit conversion from ft to m
+        public const double PSI_TO_PA = 6894.75729;                 // unit conversion from psi to kPa
+        public const double Pa_TO_Psi = 0.000145038;                // unit conversion from Pa to Psi
+        public const double DAY_TO_SEC = 86400;                     // unit conversion from day to sec
+        public const double FT_TO_IN = 12.0;                        // unit conversion from ft to inch
+        public const double G = 32.174049;                          // accelation Gravity
+        public const double BBL_TO_Scf = 5.615;                     // unit conversion from barrel to scf 
+        public const double WATER_DENSITY = 62.4;                   // lb/ft3
+
+    }
+}
---
+++ PipePressureDrop/FluidProperties.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipePressureDrop
+{
+    public class FluidProperties
+    {
+        private double m_miu_o_stc;
+        private double m_miu_w_stc;
+        private double m_miu_g_stc;
+        public FluidProperties(double miu_o_stc, double miu_w_stc, double miu_g_stc)
+        {
+            m_miu_o_stc = miu_o_stc * (1 / ConversionFactor.CP_TO_KG_MSec);
+            m_miu_w_stc = miu_w_stc * (1 / ConversionFactor.CP_TO_KG_MSec);
+            m_miu_g_stc = miu_g_stc * (1 / ConversionFactor.CP_TO_KG_MSec);
+        }
+
+        public double CalculateRs(double p)               // solution gas-oil ratio, scf/stb
+        {
+            return 0.0461 * Math.Pow(p, 1.205);
+        }
+
+        public double CalculateBo(double p)               // oil formation volume factor, bbl/stb
+        {
+            double rs = CalculateRs(p);
+            double bo = 0.972 + Math.Pow((rs * 0.00048 + 0.1026), 1.175);
+            return bo;
+        }
+
+        public double CalculateBg(double p)               // gas formation volume factor, ft3/scf
+        {
+            return 16 / p;
+        }
+        public double CalculateOilDensity(double p)       // oil density, lb/ft3
+        {
+            double rs = CalculateRs(p);
+            double bo = CalculateBo(p);
+            double rho_o = (53.04 + 0.01254 * rs) / bo;
+            return rho_o;
+        }
+
+        public double CalculateGasDensity(double p)       // gas density, lb/ft3
+        {
+            double rho_g = 0.0028 * p;
+            return rho_g;
+        }
+
+        public double CalculateWaterDensity(double p)     // water density, lb/ft3
+        {
+            return 62.4;
+        }
+
+        public double CalculateWaterViscosity(double p)   // water viscoity, cp
+        {
+            return m_miu_w_stc;
+        }
+        public double CalculateOilViscosity(double p)     // oil viscosity, cp
+        {
+            return m_miu_o_stc;
+        }
+        public double CalculateGasViscosity(double p)     // gas viscosity, cp
+        {
+            return m_miu_g_stc;
+        }
+    }
+}
