74b8b11461d9e2738b8821835f0824b214c43d8c
meysam <rahmanian.meysam655@gmail.com>
2023-02-26 18:03:04 -0700

    Pipe is Added

---
+++ PipePressureDrop/Pipe.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipePressureDrop
+
+{
+    public class Pipe
+    {
+        # region member_variables
+        private double m_length;
+        private double m_diameter;
+        private double m_inclination_angle;
+        private double m_area;
+        private double m_inletPressure;
+        public double m_log_content;
+        private PressureDropModel m_pressureDropModel;
+        # endregion
+
+        # region properties
+        public double Length
+        {
+            get { return m_length; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Length of pipe should be positive.");
+                }
+                else
+                {
+                    m_length = value;
+                }
+            }
+        }
+        public double Diameter
+        {
+            get { return m_diameter; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("Diameter of pipe should be positive.");
+                }
+                else
+                {
+                    m_diameter = value;
+                }
+            }
+        }
+
+        public double InclinationAngle
+        {
+            get { return m_inclination_angle; }
+            set
+            {
+                if (value < 0 || value > 360)
+                {
+                    throw new Exception("Inclination angle of pipe should be in the range of 0 and 360.");
+                }
+                else
+                {
+                    m_inclination_angle = value;
+                }
+            }
+        }
+
+        public double Area
+        {
+            get { return m_area; }
+        }
+
+        public double InletPressure
+        {
+            get { return m_inletPressure; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("InletPressure of pipe should be positive.");
+                }
+                else
+                {
+                    m_inletPressure = value;
+                }
+            }
+        }
+
+        public PressureDropModel PressureDropModel
+        {
+            get { return m_pressureDropModel; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Pressure Model Can Not be null");
+                }
+                else
+                {
+                    m_pressureDropModel = value;
+                }
+            }
+        }
+        # endregion
+
+        # region constructors
+        public Pipe(double length, double diameter, double inclination_angle, PressureDropModel pressureDropModel)
+        {
+            Length = length;
+            Diameter = diameter;
+            InclinationAngle = inclination_angle;
+            PressureDropModel = pressureDropModel;
+            m_area = (Math.PI * Math.Pow(m_diameter, 2) / 4.0);
+        }
+
+        public Pipe(Pipe origPipe)
+        {
+            this.Length = origPipe.Length;
+            this.Diameter = origPipe.Diameter;
+            this.InclinationAngle = origPipe.InclinationAngle;
+            this.PressureDropModel = origPipe.PressureDropModel;
+            this.m_area = origPipe.Area;
+        }
+
+        #endregion
+
+        # region method
+        public double CalculateOutletPressure(double inletPress, double qo, double qg, double qw, ref string m_log_content,
+                                              ref List<double> xlist, ref List<double> plist, double numSeg = 1)
+        {
+
+            double len_seg = Length / numSeg;
+            double seg_pin = inletPress;
+            double pout_seg = inletPress;
+            //m_log_content += "Calculation started ...!" + System.Environment.NewLine;
+
+            for (int i_seg = 0; i_seg < numSeg; i_seg++)
+            {
+                pout_seg = m_pressureDropModel.CalculatePressureLoss(seg_pin, m_area, Diameter, InclinationAngle, len_seg, qo, qg, qw);
+                plist.Add(pout_seg / 1000.0);
+                double x = xlist.Last() + len_seg;
+                xlist.Add(x);
+                seg_pin = pout_seg;
+                m_log_content += $"pressure drop calculations in seg {i_seg + 1} converged:" + System.Environment.NewLine;
+                m_log_content += $"     X: {x} (m), P:  {pout_seg / 1000.0} (kPa)" + System.Environment.NewLine;
+
+
+            }
+            m_log_content += "-----------------------------------------------" + System.Environment.NewLine;
+            return pout_seg;
+        }
+        #endregion
+    }
+}
---
+++ PipePressureDrop/PressureDropModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipePressureDrop
+{
+    public class PressureDropModel
+    {
+        #region member variables
+        private FluidProperties m_fluidProperties;
+        #endregion
+
+        #region properties
+        public FluidProperties FProperties
+        {
+            get { return m_fluidProperties; }
+
+        }
+
+        #endregion
+
+
+        #region constructors
+        public PressureDropModel(FluidProperties fluidProperties)
+        {
+            m_fluidProperties = fluidProperties;
+
+        }
+
+        #endregion
+
+
+        #region methods
+        public double CalculatePressureLoss(double inletPress, double area, double diameter, double angle, double length, double inletOilFlowRate, double inletGasFlowRate, double inletWaterFlowRate)
+        {
+            // Units are converted to be compatible with the corrolations.
+            double length_ft = length * (1.0 / ConversionFactor.FT_TO_M);
+            double area_ft2 = area * (1.0 / Math.Pow(ConversionFactor.FT_TO_M, 2));
+            double diameter_ft = diameter * (1.0 / ConversionFactor.FT_TO_M);
+            double qo_ft3_per_s = inletOilFlowRate * (1.0 / (ConversionFactor.DAY_TO_SEC * ConversionFactor.FT3_TO_M3));
+            double qw_ft3_per_s = inletWaterFlowRate * (1.0 / (ConversionFactor.DAY_TO_SEC * ConversionFactor.FT3_TO_M3));
+            double qg_ft3_per_s = inletGasFlowRate * (1.0 / (ConversionFactor.DAY_TO_SEC * ConversionFactor.FT3_TO_M3));
+            double pin_psi = inletPress * ConversionFactor.Pa_TO_Psi;
+            double fo = qo_ft3_per_s / (qw_ft3_per_s + qo_ft3_per_s);
+            double pout_psi = pin_psi;
+
+            double pout_err = 1.0;
+
+            while (pout_err > 0.05)
+            {
+                double p_ave_psi = (pin_psi + pout_psi) / 2.0;
+                double rho_o = m_fluidProperties.CalculateOilDensity(p_ave_psi);
+                double rho_w = m_fluidProperties.CalculateWaterDensity(p_ave_psi);
+                double rho_g = m_fluidProperties.CalculateGasDensity(p_ave_psi);
+                double rho_l = (fo * rho_o) + rho_w * (1.0 - fo);
+                double miu_w = m_fluidProperties.CalculateWaterViscosity(p_ave_psi);
+                double miu_g = m_fluidProperties.CalculateGasViscosity(p_ave_psi);
+                double miu_o = m_fluidProperties.CalculateOilViscosity(p_ave_psi);
+                double miu_l = (fo * miu_o) + miu_w * (1.0 - fo);
+                double bo = m_fluidProperties.CalculateBo(p_ave_psi);
+                double vso = qo_ft3_per_s * bo / area_ft2;
+                double vsw = qw_ft3_per_s / area_ft2;
+                double vsl = (fo * vso) + vsw * (1.0 - fo);
+                double gor = qg_ft3_per_s / qo_ft3_per_s;
+                double rs = m_fluidProperties.CalculateRs(p_ave_psi);
+                double bg = m_fluidProperties.CalculateBg(p_ave_psi);
+                double vsg = qo_ft3_per_s * (gor - rs / ConversionFactor.BBL_TO_Scf) * bg / area_ft2;                    // Rs is dimentionless here
+                double vm = vsl + vsg;
+                double lambda = vsl / vm;
+                double rho_ns = rho_l * lambda + rho_g * (1.0 - lambda);
+                double miu_ns = miu_l * lambda + miu_g * (1.0 - lambda);
+
+                double miu_ns_convert = miu_ns * ConversionFactor.CP_TO_LB_FTSec;
+                double re_num = rho_ns * vm * diameter_ft / miu_ns_convert;
+
+                double fric = 0.0056 + 0.5 * Math.Pow(re_num, -0.32);
+                double angle_rad = angle * 2 * Math.PI / 360.0;
+                double dp_hyd = length_ft * rho_ns * Math.Sin(angle_rad) / Math.Pow(ConversionFactor.FT_TO_IN, 2);
+                double dp_fric = length_ft * (fric * rho_ns * vm * Math.Abs(vm) / (2.0 * diameter_ft)) / Math.Pow(ConversionFactor.FT_TO_IN, 2) / ConversionFactor.G;
+                double dp = dp_hyd + dp_fric;
+                double pout_psi_new = pin_psi - dp;
+                pout_err = Math.Abs(pout_psi - pout_psi_new) / pout_psi * 100.0;
+                pout_psi = pout_psi_new;
+            }
+
+            double dp_out_pa = pout_psi * ConversionFactor.PSI_TO_PA;
+            double pout = dp_out_pa;
+
+            return pout;
+        }
+
+        #endregion
+
+    }
+}
