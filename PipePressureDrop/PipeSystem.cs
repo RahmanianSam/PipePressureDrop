8c4ef9dda03d96ede261d0acbb83ac32b3136783
meysam <rahmanian.meysam655@gmail.com>
2023-02-26 18:05:32 -0700

    PipeSystemAdded

---
+++ PipePressureDrop/PipeSystem.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipePressureDrop
+{
+    public class PipeSystem
+    {
+        #region member variable
+
+        private List<Pipe> m_pipeList;
+
+
+        #endregion
+
+        #region properties
+        public List<Pipe> PipeList
+        {
+            get { return m_pipeList; }
+            set { m_pipeList = value; }
+        }
+        #endregion
+        public double m_pout;
+
+
+        #region constructor
+        public PipeSystem()
+        {
+            PipeList = new List<Pipe>();
+        }
+        #endregion
+
+        #region method
+        public void Add(Pipe p)
+        {
+            PipeList.Add(p);
+
+        }
+        public double CalculatePipeSystem_POut(double inletPress, double qo, double qg, double qw, ref string m_log_content,
+            ref List<double> xlist, ref List<double> plist, double numSeg)
+        {
+            double m_pin_pipe = inletPress;
+            double m_pout_pipe = inletPress;
+            //m_log_content += "Calculation  started ...!" + System.Environment.NewLine;
+            int i = 0;
+            foreach (Pipe p in PipeList)
+            {
+
+                i++;
+                m_log_content += $"Calculations in Pipe {i} Started!!" + System.Environment.NewLine;
+                m_log_content += "-----------------------------------------------" + System.Environment.NewLine;
+                m_pin_pipe = m_pout_pipe;
+                m_pout_pipe = p.CalculateOutletPressure(m_pin_pipe, qo, qg, qw, ref m_log_content,
+                                              ref xlist, ref plist, numSeg);
+
+
+            }
+            m_log_content += "Calculation ended...! " + System.Environment.NewLine;
+            return m_pout_pipe;
+        }
+        #endregion
+    }
+
+}
